215367d2ab885dd5c2acaaf7761b592346a36bde
AdKowalewski <a.kowalewski.student@gmail.com>
2023-04-07 09:26:34 +0200

    Add modal for deleting image

--- force-app/main/default/lwc/previewImage/previewImage.js
+++ force-app/main/default/lwc/previewImage/previewImage.js
@@ -1,4 +1,4 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import fileDelete from '@salesforce/apex/FileController.deleteFile';
 import getDisplayUrl from '@salesforce/apex/FileController.getProductDisplayUrl';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
@@ -13,6 +13,8 @@ export default class PreviewImage extends LightningElement {
     @api file;
     @api recordId;
     @api thumbnail;
+    @track showDeleteModal = false;
+    @track currentRecordId;
     dispUrl = '';
 
     get iconName() {
@@ -21,31 +23,9 @@ export default class PreviewImage extends LightningElement {
 
     async handleSelect(event) {
         let selectedVal = event.detail.value;
-        let currentRecordId = event.currentTarget.dataset.id;
+        this.currentRecordId = event.currentTarget.dataset.id;
         if(selectedVal === 'delete') {
-            let defUrl = '/sfc/servlet.shepherd/version/renditionDownload?rendition=THUMB720BY480&versionId=' + this.file.Id + '&operationContext=CHATTER&contentId=' + this.file.ContentDocumentId;
-            await getDisplayUrl({id: this.recordId})
-                .then(result => {
-                    let data = JSON.parse(result);
-                    this.dispUrl = data.message;
-                })
-            if(defUrl == this.dispUrl) {
-                await updateRecord({
-                    fields: {
-                        [PRODUCT2_ID_FIELD.fieldApiName]: this.recordId,
-                        [DEFAULT_IMAGE_URL.fieldApiName]: null
-                    }
-                });
-            }
-            await fileDelete({recordId: currentRecordId});
-            this.dispatchEvent(new CustomEvent('deletefile'));
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Success!',
-                    message: 'Image was deleted successfully.',
-                    variant: 'success'
-                })
-            );
+            this.showDeleteModal = true;
         } else if(selectedVal === 'default') {
             await updateRecord({
                 fields: {
@@ -66,5 +46,36 @@ export default class PreviewImage extends LightningElement {
     filePreview() {
         const showPreview = this.template.querySelector('c-preview-file-modal');
         showPreview.show();
+    }
+
+    handleCancel() {
+        this.showDeleteModal = false;
+    }
+
+    async handleDelete() {
+        let defUrl = '/sfc/servlet.shepherd/version/renditionDownload?rendition=THUMB720BY480&versionId=' + this.file.Id + '&operationContext=CHATTER&contentId=' + this.file.ContentDocumentId;
+        await getDisplayUrl({id: this.recordId})
+            .then(result => {
+                let data = JSON.parse(result);
+                this.dispUrl = data.message;
+            })
+        if(defUrl == this.dispUrl) {
+            await updateRecord({
+                fields: {
+                    [PRODUCT2_ID_FIELD.fieldApiName]: this.recordId,
+                    [DEFAULT_IMAGE_URL.fieldApiName]: null
+                }
+            });
+        }
+        await fileDelete({recordId: this.currentRecordId});
+        this.showDeleteModal = false;
+        this.dispatchEvent(new CustomEvent('deletefile'));
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success!',
+                message: 'Image was deleted successfully.',
+                variant: 'success'
+            })
+        );
     }
 }
