14bd790b10b5d95920f83353dffef9ec3fd5d650
AdKowalewski <a.kowalewski.student@gmail.com>
2023-05-07 11:31:46 +0200

    changes

--- force-app/main/default/lwc/productDetails/productDetails.js
+++ force-app/main/default/lwc/productDetails/productDetails.js
@@ -8,6 +8,20 @@ export default class ProductDetails extends LightningElement {
     @track product;
     @api productId;
     @track userId = UserId;
+    @track myEvent;
+    @track productEvents = [];
+    @track hours = [
+        '9:00','9:30',
+        '10:00','10:30',
+        '11:00','11:30',
+        '12:00','12:30',
+        '13:00','13:30',
+        '14:00','14:30',
+        '15:00','15:30',
+        '16:00','16:30',
+    ];
+    @track chosenDate;
+    @track chosenHour;
 
     connectedCallback() {
         getProduct({id: this.productId})
