ce5302141d7fadf4bf68605cd7aab31f9e078eee
AdKowalewski <a.kowalewski.student@gmail.com>
2023-05-11 15:22:24 +0200

    changes

---
+++ force-app/main/default/lwc/myMeetings/myMeetings.js
@@ -0,0 +1,57 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import UserId from '@salesforce/user/Id';
+import userEvents from '@salesforce/apex/ProductController.getAllUserEvents';
+import eventDelete from '@salesforce/apex/ProductController.deleteEvent';
+
+export default class MyMeetings extends LightningElement {
+    
+    @track userEvents = [];
+    @track userId = UserId;
+    @track idToDelete;
+    @track displayModal = false;
+
+    get areMeetings() {
+        let flag = false;
+        if(this.userEvents.length != 0) {
+            flag = true;
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
+
+    connectedCallback() {
+        userEvents({whoId: this.userId})
+            .then(result => {
+                this.myEvent = JSON.parse(result);
+            })
+    }
+
+    hideModal() {
+        this.displayModal = false;
+    }
+
+    showModal(event) {
+        this.idToDelete = event.target.dataset.deleteId;
+        this.displayModal = true;
+    }
+
+    handleDelete(event) {
+        eventDelete({id: this.idToDelete})
+            .then(result => {
+                this.displayModal = false;
+                userEvents({whoId: this.userId})
+                    .then(result => {
+                        this.myEvent = JSON.parse(result);
+                    })
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Meeting cancelled successfully',
+                        variant: 'success'
+                    })
+                );
+            })
+    }
+}
