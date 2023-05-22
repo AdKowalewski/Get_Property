3305b9b2fad2326753b657c98d4e21e33f0907ee
AdKowalewski <a.kowalewski.student@gmail.com>
2023-05-22 10:07:28 +0200

    changes

--- force-app/main/default/lwc/productDetails/productDetails.js
+++ force-app/main/default/lwc/productDetails/productDetails.js
@@ -136,64 +136,7 @@ export default class ProductDetails extends LightningElement {
     @track isNotEmpty;
 
     connectedCallback() {
-        // this.getProductMethod();
-
-        getProduct({id: this.productId})
-            .then(result => {
-                this.product = JSON.parse(result);
-                if(this.product.wifi == true) {
-                    this.product.wifi = yes;
-                } else {
-                    this.product.wifi = no;
-                }
-                if(this.product.parking == true) {
-                    this.product.parking = yes;
-                } else {
-                    this.product.parking = no;
-                }
-                if(this.product.elevator == true) {
-                    this.product.elevator = yes;
-                } else {
-                    this.product.elevator = no;
-                }
-                if(this.product.kitchen == true) {
-                    this.product.kitchen = yes;
-                } else {
-                    this.product.kitchen = no;
-                }
-                oppsCheck({whatId: this.product.id})
-                    .then(result => {
-                        this.isNotEmpty = JSON.stringify(result);
-                    })
-                userEvent({whoId: this.userId, whatId: this.product.id})
-                    .then(result => {
-                        this.myEvent = JSON.parse(result);
-                    })
-                userReservation({whoId: this.userId, whatId: this.product.id})
-                    .then(result => {
-                        this.myRes = JSON.parse(result);
-                        this.newprice = this.product.price - this.myRes.price;
-                    })
-                productEvents({whatId: this.product.id, start: this.meetingDate})
-                    .then(result => {
-                        if(result) {
-                            let data = JSON.parse(result);
-                            this.productEvents = data.events;
-                            this.availableHours = data.hours;
-                            for(let j = 0; j < this.hours.length; j++) {
-                                this.hours[j].class = 'hourContainer2';
-                            }
-                            for(let i = 0; i < this.availableHours.length; i++) {
-                                for(let j = 0; j < this.hours.length; j++) {
-                                    if(this.hours[j].h == this.availableHours[i]) {
-                                        this.hours[j].class = 'hourContainer1';
-                                        continue;
-                                    }
-                                }
-                            }
-                        }            
-                    })
-            })
+        this.getProductMethod();
     }
 
     get hasAnyMeetings() {
@@ -282,49 +225,14 @@ export default class ProductDetails extends LightningElement {
         }
     }
 
-    // handleMeetingDateChange(event) {
-    //     this.meetingDate = event.target.value;
-    //     let dd = new Date(new Date().getTime()).toISOString();
-    //     if(this.meetingDate >= dd) {
-    //         this.productEventsMethod();
-    //     } else if(this.meetingDate < dd) {
-    //         this.meetingDate = new Date(new Date().getTime() + (1*24*60*60*1000)).toISOString();
-    //         this.toastMethod(error, dateerror, 'error');
-    //     }
-    // }
-
     handleMeetingDateChange(event) {
         this.meetingDate = event.target.value;
         let dd = new Date(new Date().getTime()).toISOString();
         if(this.meetingDate >= dd) {
-            productEvents({whatId: this.product.id, start: this.meetingDate})
-                .then(result => {
-                    if(result) {
-                        let data = JSON.parse(result);
-                        this.productEvents = data.events;
-                        this.availableHours = data.hours;
-                        for(let j = 0; j < this.hours.length; j++) {
-                            this.hours[j].class = 'hourContainer2';
-                        }
-                        for(let i = 0; i < this.availableHours.length; i++) {
-                            for(let j = 0; j < this.hours.length; j++) {
-                                if(this.hours[j].h == this.availableHours[i]) {
-                                    this.hours[j].class = 'hourContainer1';
-                                    continue;
-                                }
-                            }
-                        }
-                    }            
-                })
+            this.productEventsMethod();
         } else if(this.meetingDate < dd) {
             this.meetingDate = new Date(new Date().getTime() + (1*24*60*60*1000)).toISOString();
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: error,
-                    message: dateerror,
-                    variant: 'error'
-                })
-            );
+            this.toastMethod(error, dateerror, 'error');
         }
     }
 
@@ -345,69 +253,16 @@ export default class ProductDetails extends LightningElement {
         this.showResModal = true;
     }
 
-    // displayEventModal() {
-    //     this.showEventModal = true;
-    //     this.productEventsMethod();
-    // }
-
     displayEventModal() {
         this.showEventModal = true;
-        productEvents({whatId: this.product.id, start: this.meetingDate})
-            .then(result => {
-                if(result) {
-                    let data = JSON.parse(result);
-                    this.productEvents = data.events;
-                    this.availableHours = data.hours;
-                    for(let j = 0; j < this.hours.length; j++) {
-                        this.hours[j].class = 'hourContainer2';
-                    }
-                    for(let i = 0; i < this.availableHours.length; i++) {
-                        for(let j = 0; j < this.hours.length; j++) {
-                            if(this.hours[j].h == this.availableHours[i]) {
-                                this.hours[j].class = 'hourContainer1';
-                                continue;
-                            }
-                        }
-                    }
-                }            
-            })
+        this.productEventsMethod();
     }
-
-    // handleCreateEvent(event) {
-    //     let dd = new Date(new Date().getTime()).toISOString();
-    //     if(this.meetingDate >= dd) {
-    //         let hclass = event.target.dataset.class;
-    //         if(hclass == hourcontainer1) {
-    //             let chosenHour = event.target.dataset.hour.split(':');
-    //             let meetData = this.meetingDate.split('-');
-    //             eventCreate({
-    //                 agentId: this.product.agentId, 
-    //                 whoId: this.userId, 
-    //                 whatId: this.product.id, 
-    //                 location: this.product.address,
-    //                 year: parseInt(meetData[0]),
-    //                 month: parseInt(meetData[1]),
-    //                 day: parseInt(meetData[2]),
-    //                 hour: parseInt(chosenHour[0]) + 2,
-    //                 minute: chosenHour[1] == '30' ? 30 : 0})
-    //                 .then(result => {
-    //                     this.userEventMethod();
-    //                     this.productEventsMethod();
-    //                     this.showEventModal = false;
-    //                     this.toastMethod(success, meetingsuccess, 'success');
-    //                 })
-    //         }
-    //     } else if(this.meetingDate < dd) {
-    //         this.meetingDate = new Date(new Date().getTime() + (1*24*60*60*1000)).toISOString();
-    //         this.toastMethod(error, dateerror, 'error');
-    //     }
-    // }
 
     handleCreateEvent(event) {
         let dd = new Date(new Date().getTime()).toISOString();
         if(this.meetingDate >= dd) {
             let hclass = event.target.dataset.class;
-            if(hclass == 'hourContainer1') {
+            if(hclass == hourcontainer1) {
                 let chosenHour = event.target.dataset.hour.split(':');
                 let meetData = this.meetingDate.split('-');
                 eventCreate({
@@ -421,191 +276,41 @@ export default class ProductDetails extends LightningElement {
                     hour: parseInt(chosenHour[0]) + 2,
                     minute: chosenHour[1] == '30' ? 30 : 0})
                     .then(result => {
-                        userEvent({whoId: this.userId, whatId: this.product.id})
-                            .then(result => {
-                                this.myEvent = JSON.parse(result);
-                            })
-                        productEvents({whatId: this.product.id, start: this.meetingDate})
-                            .then(result => {
-                                if(result) {
-                                    let data = JSON.parse(result);
-                                    this.productEvents = data.events;
-                                    this.availableHours = data.hours;
-                                    for(let j = 0; j < this.hours.length; j++) {
-                                        this.hours[j].class = 'hourContainer2';
-                                    }
-                                    for(let i = 0; i < this.availableHours.length; i++) {
-                                        for(let j = 0; j < this.hours.length; j++) {
-                                            if(this.hours[j].h == this.availableHours[i]) {
-                                                this.hours[j].class = 'hourContainer1';
-                                                continue;
-                                            }
-                                        }
-                                    }
-                                }            
-                            })
+                        this.userEventMethod();
+                        this.productEventsMethod();
                         this.showEventModal = false;
-                        this.dispatchEvent(
-                            new ShowToastEvent({
-                                title: success,
-                                message: meetingsuccess,
-                                variant: 'success'
-                            })
-                        );
+                        this.toastMethod(success, meetingsuccess, 'success');
                     })
             }
         } else if(this.meetingDate < dd) {
             this.meetingDate = new Date(new Date().getTime() + (1*24*60*60*1000)).toISOString();
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: error,
-                    message: dateerror,
-                    variant: 'error'
-                })
-            );
+            this.toastMethod(error, dateerror, 'error');
         }
     }
-
-    // handleCancelMeeting() {
-    //     eventDelete({id: this.myEvent.id})
-    //         .then(result => {
-    //             this.myEvent = null;
-    //             this.userEventMethod();
-    //             this.productEventsMethod();
-    //             this.toastMethod(success, meetingcancelsuccess, 'success');
-    //         })
-    // }
 
     handleCancelMeeting() {
         eventDelete({id: this.myEvent.id})
             .then(result => {
                 this.myEvent = null;
-                userEvent({whoId: this.userId, whatId: this.product.id})
-                    .then(result => {
-                        this.myEvent = JSON.parse(result);
-                    })
-                    productEvents({whatId: this.product.id, start: this.meetingDate})
-                        .then(async result => {
-                            if(result) {
-                                let data = JSON.parse(result);
-                                this.productEvents = data.events;
-                                this.availableHours = data.hours;
-                                for(let j = 0; j < this.hours.length; j++) {
-                                    this.hours[j].class = 'hourContainer2';
-                                }
-                                for(let i = 0; i < this.availableHours.length; i++) {
-                                    for(let j = 0; j < this.hours.length; j++) {
-                                        if(this.hours[j].h == this.availableHours[i]) {
-                                            this.hours[j].class = 'hourContainer1';
-                                            continue;
-                                        }
-                                    }
-                                }
-                            }            
-                        })
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: success,
-                        message: meetingcancelsuccess,
-                        variant: 'success'
-                    })
-                );
+                this.userEventMethod();
+                this.productEventsMethod();
+                this.toastMethod(success, meetingcancelsuccess, 'success');
             })
     }
-
-    // saveReservation() {
-    //     reservationCreate({whatId: this.product.id, whoId: this.userId, agentId: this.product.agentId, noDays: this.resPeriod, userId: this.userId})
-    //         .then(result => {
-    //             this.getProductMethod();
-    //             this.showResModal = false;
-    //             this.toastMethod(success, reservationsuccess, 'success');
-    //         })
-    // }
-
-    // quoteCreation() {
-    //     quoteCreate({whatId: this.product.id, whoId: this.userId, agentId: this.product.agentId, userId: this.userId})
-    //         .then(result => {
-    //             this.oppsCheckMethod();
-    //         })
-    // }
 
     saveReservation() {
         reservationCreate({whatId: this.product.id, whoId: this.userId, agentId: this.product.agentId, noDays: this.resPeriod, userId: this.userId})
             .then(result => {
-                getProduct({id: this.productId})
-                    .then(result => {
-                        this.product = JSON.parse(result);
-                        if(this.product.wifi == true) {
-                            this.product.wifi = yes;
-                        } else {
-                            this.product.wifi = no;
-                        }
-                        if(this.product.parking == true) {
-                            this.product.parking = yes;
-                        } else {
-                            this.product.parking = no;
-                        }
-                        if(this.product.elevator == true) {
-                            this.product.elevator = yes;
-                        } else {
-                            this.product.elevator = no;
-                        }
-                        if(this.product.kitchen == true) {
-                            this.product.kitchen = yes;
-                        } else {
-                            this.product.kitchen = no;
-                        }
-                        oppsCheck({whatId: this.product.id})
-                            .then(result => {
-                                this.isNotEmpty = JSON.stringify(result);
-                            })
-                        userEvent({whoId: this.userId, whatId: this.product.id})
-                            .then(result => {
-                                this.myEvent = JSON.parse(result);
-                            })
-                        userReservation({whoId: this.userId, whatId: this.product.id})
-                            .then(result => {
-                                this.myRes = JSON.parse(result);
-                                this.newprice = this.product.price - this.myRes.price;
-                            })
-                        productEvents({whatId: this.product.id, start: this.meetingDate})
-                            .then(result => {
-                                if(result) {
-                                    let data = JSON.parse(result);
-                                    this.productEvents = data.events;
-                                    this.availableHours = data.hours;
-                                    for(let j = 0; j < this.hours.length; j++) {
-                                        this.hours[j].class = hourcontainer2;
-                                    }
-                                    for(let i = 0; i < this.availableHours.length; i++) {
-                                        for(let j = 0; j < this.hours.length; j++) {
-                                            if(this.hours[j].h == this.availableHours[i]) {
-                                                this.hours[j].class = hourcontainer1;
-                                                continue;
-                                            }
-                                        }
-                                    }
-                                }            
-                            })
-                    })
+                this.getProductMethod();
                 this.showResModal = false;
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: success,
-                        message: reservationsuccess,
-                        variant: 'success'
-                    })
-                );
+                this.toastMethod(success, reservationsuccess, 'success');
             })
     }
 
     quoteCreation() {
         quoteCreate({whatId: this.product.id, whoId: this.userId, agentId: this.product.agentId, userId: this.userId})
             .then(result => {
-                oppsCheck({whatId: this.product.id})
-                    .then(result => {
-                        this.isNotEmpty = JSON.stringify(result);
-                    })
+                this.oppsCheckMethod();
             })
     }
 
